5493e2a91e83f1bafcab8550ff77aa0879e55bbd
prasad-585 <gvskprasad585@gmail.com>
2026-03-13 11:24:27 +0530

    New files

---
+++ demo.py
@@ -0,0 +1,50 @@
+# install libraries
+# import library
+
+
+# read image 
+
+import numpy as np
+import cv2
+inp1=cv2.imread('amn.jpeg')
+# print(inp1)
+cv2.imshow("my image",inp1)
+# cv2.waitKey(0)
+
+# resize image
+
+# inpr=cv2.resize(inp1,(1600,700))
+# # cv2.imshow('resize image',inpr)
+# print(inpr.shape)
+# cv2.waitKey(0)
+
+# image conversion
+
+# inpgray=cv2.cvtColor(inpr,cv2.COLOR_BGR2GRAY)
+# cv2.imshow('gray image',inpgray)
+# cv2.waitKey(0)
+# inprgb = cv2.cvtColor(inpr,cv2.COLOR_BGR2RGB)
+# # cv2.imshow('rgb image',inprgb)
+# cv2.waitKey(0)
+
+# example for video
+
+# import cv2
+# import numpy as np
+# cap=cv2.VideoCapture("video.mp4")
+# cap=cv2.VideoCapture(0)
+# while True:
+#     ret,frame=cap.read()
+#     cv2.imshow('input video',frame)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+
+#     # resize video
+
+#     inpr=cv2.resize(frame,(600,500))
+#     cv2.imshow('Resize image',inpr)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+
+
+    
---
+++ shohel.py
@@ -0,0 +1,35 @@
+# name = input("Enter something:")
+# r =""
+# for i in name:
+#     if i != "":
+#         r=i+r
+# print(r)
+
+# largest = 0
+# for i in range(3):
+#     num = int(input("Enter number : "))
+
+#     if num < 0:
+#         print("No less then 0")
+#         break
+#     else:    
+#         if num > largest:
+#             largest = num
+# print("largest no : ",largest)
+
+
+# n=int(input())
+# for n in range(1,m+1):
+#     if n%3==0 and n%5==0:
+#         print("fizzbuzz")
+#     elif n%3==0 and n%5!=0:
+#         print("fizz")
+#     elif n%5==0 and n%3!=0:
+#         print("buzz")
+#     else:
+#         print(n)
+
+
+
+a=int(input("enter the number:"))
+print("True" if a==10 else "False")
